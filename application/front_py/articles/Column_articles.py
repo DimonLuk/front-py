b97e9b417f62c622d9170567ccd6d92cc38458b6
dimonluk <lds4ever2000@gmail.com>
2018-02-01 19:43:45 +0200

    pep8

--- application/front_py/articles/Column_articles.py
+++ application/front_py/articles/Column_articles.py
@@ -1,11 +1,11 @@
 """
 Copyright (C) 2018  Dima Lukashov github.com/DimonLuk
-    
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-      
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -18,35 +18,42 @@ from core import elements as e
 from containers import Section_container
 from rows import Block_row
 from defaults import Header
+
+
 class Column_articles(Section_container):
-    def __init__(self,header="",footer="",headers_level=1,position="",vertical_distance="30px",horizontal_line=False,*articles):
+    def __init__(
+            self, header="", footer="", headers_level=1, position="",
+            vertical_distance="30px", horizontal_line=False, *articles):
         super().__init__()
         self.header = header
         self.headers_level = headers_level
         self.line = horizontal_line
         self.footer = footer
         self.row_header = Block_row()
-        
-        self.head = Header(self.headers_level,header)
+
+        self.head = Header(self.headers_level, header)
         if position == "center":
             self.head._add_class("mx-auto")
         self.row_header.add_content(self.head)
         if vertical_distance:
-            self.row_header._add_style({"margin-bottom":vertical_distance})
+            self.row_header._add_style({"margin-bottom": vertical_distance})
         super().add_content(self.row_header)
-        
+
         self.rowArticles = Block_row()
         if articles:
             self.rowArticles.add_content(*articles)
-    def add_content(self,*content):
+
+    def add_content(self, *content):
         self.rowArticles.add_content(*content)
+
     def _render(self):
-            super().add_content(self.rowArticles)
-            if self.footer:
-                super().add_content(self.footer)
-            if self.line:
-                super().add_content(e._Horizontal_line_element())
-            super()._render()
-    def __call__(self,*articles):
+        super().add_content(self.rowArticles)
+        if self.footer:
+            super().add_content(self.footer)
+        if self.line:
+            super().add_content(e._Horizontal_line_element())
+        super()._render()
+
+    def __call__(self, *articles):
         for i in articles:
             self.add_content(i)
--- application/front_py/core/constants.py
+++ application/front_py/core/constants.py
@@ -1,11 +1,11 @@
 """
 Copyright (C) 2018  Dima Lukashov github.com/DimonLuk
-    
+
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-      
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -21,4 +21,5 @@ SCRIPT_JS = "9a9569e9d73f33740eada95275da7f30"
 BOOTSTRAP_CSS = "e3202aea761d3d587dfcfc43c6982565"
 
 INVALID_INSERTION_MESSAGE = "You can't use '%s', please try some object from the framework library"
-AVALIABLE_FORMATS = ("jpg","png","svg","map","ico","js","css") #Formats of files that can be loaded automatically from pages/media folder
+# Formats of files that can be loaded automatically from pages/media folder
+AVALIABLE_FORMATS = ("jpg", "png", "svg", "map", "ico", "js", "css")
